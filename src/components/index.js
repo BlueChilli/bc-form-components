4f005a2805401d606dd4c6cc7ab413619b975119
Harry <harry.aydin.23@gmail.com>
2019-01-17 15:08:55 +1100

    Fix export?

--- src/components/index.js
+++ src/components/index.js
@@ -2,8 +2,8 @@ import DateInputField from "../components/DateInputField/DateInputField";
 import TextareaField from "../components/TextareaField/TextareaField";
 import CheckboxField from "../components/CheckboxField/CheckboxField";
 
-module.exports = {
+export default {
   DateInputField,
   TextareaField,
   CheckboxField
-}
+};
